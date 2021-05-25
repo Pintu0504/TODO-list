23a3244171f179ffe84232dc0659964af9035eb4
Pintu kumar <Pintu.kumar@gilbarco.com>
2021-05-25 12:50:17 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, jsonify, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+from werkzeug.utils import redirect
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db=SQLAlchemy(app) 
+
+class TODO(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow) 
+
+    def __repr__(self) -> str:
+        return f"{self.sno}-{self.title}"
+
+@app.route('/', methods=['GET', 'POST'])
+def Home():
+    if request.method == 'POST':
+        title=request.form['title']
+        desc=request.form['desc']
+        todo = TODO(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+    allTodo = TODO.query.all()
+    return render_template('index.html', allTodo=allTodo)
+
+@app.route('/show')
+def show():
+    allTodo = TODO.query.all()
+    print(allTodo)
+    return "This is page to show contents"
+
+
+@app.route('/update/<int:sno>',  methods=['GET', 'POST'])
+def update(sno):
+    if request.method == 'POST':
+        todo = TODO.query.filter_by(sno=sno).first()
+        title = request.form['title']
+        desc = request.form['desc']
+        todo.title = title
+        todo.desc = desc
+        todo.time_created = datetime.utcnow
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/')
+    todo = TODO.query.filter_by(sno=sno).first()
+    return render_template('update.html', todo=todo)
+    
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = TODO.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
